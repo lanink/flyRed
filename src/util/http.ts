ef2674a499778912b5b944e3b91e1b52981fd350
还不如一只猪威武 <liamxin@yeah.net>
2021-05-09 16:38:25 +0800

    ignore files

---
+++ src/util/http.ts
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import { MessageBox, Loading } from 'element-ui'
+
+let loading: any;
+
+const startLoading = () => {
+    loading = Loading.service({
+        lock: true,
+        text: '加载中……',
+        background: 'rgba(0, 0, 0,0)'
+    });
+};
+
+const service = axios.create({
+    baseURL: 'https://api.apilyzy.com/api.php/provide/vod/',
+    timeout: 5000,
+    headers: { 'Content-Type': 'application/json;charset=UTF-8' }
+});
+
+service.interceptors.request.use(
+    config => {
+        if (config.method === 'post') {
+            config.data = JSON.stringify(config.data)
+        }
+        startLoading();
+        return config;
+    },
+
+    error => {
+        console.log(error);
+        return Promise.reject();
+    }
+);
+
+service.interceptors.response.use(
+    response => {
+        loading.close();
+        if (response.status === 200) {
+            if (response.data.code != 1) {
+                MessageBox.alert('出了点小问题，请联系管理员');
+            } 
+            return response.data;
+        } else {
+            Promise.reject();
+        }
+    },
+    error => {
+        console.log(error);
+        return Promise.reject();
+    }
+);
+
+// 发送post请求
+export function P(url: string, params?: any) {
+
+    return new Promise((resolve, reject) => {
+        service.post(url, params)
+            .then(res => {
+                resolve(res);
+            }, err => {
+                reject(err);
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+// 发送get请求
+export function G(url: string, params?: any) {
+    return new Promise((resolve, reject) => {
+        service.get(url, { params })
+            .then(res => {
+                resolve(res)
+            }, err => {
+                reject(err)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
